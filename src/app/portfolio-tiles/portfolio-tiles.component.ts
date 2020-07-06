124d7c8074039d06e5a088067b7525fa43a0eaa9
Bruno Sala <connect@brunosala.com>
2020-07-06 14:49:03 -0500

    sort by tag, added portfolio items

--- src/app/portfolio-tiles/portfolio-tiles.component.ts
+++ src/app/portfolio-tiles/portfolio-tiles.component.ts
@@ -8,11 +8,30 @@ import * as strapiData from '../../assets/strapi-build.json';
 })
 export class PortfolioTilesComponent implements OnInit {
 
-  strapi: any = strapiData;
+  strapiJSON:any = strapiData;
+  strapi:any;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const items = Object.entries(this.strapiJSON.data.portfolioItems);
+    let stripped:any = [];
+    
+    for (var item of items) {
+      stripped.push(item[1]);
+    }
 
+    const sorted = stripped.sort(function(a: { tag: any; },b: { tag: any; }):any {
+      const stringA = a.tag;
+      const stringB = b.tag;
+
+      return stringA > stringB ? -1 : stringA < stringB ? 1 : 0;
+    });
+
+    console.log(items);
+    console.log(stripped);
+    console.log(sorted);
+
+    this.strapi = sorted; 
+  }
 }
