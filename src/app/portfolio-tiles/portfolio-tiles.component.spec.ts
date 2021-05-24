177e97da20cf848da7348afae0908f9bb9fd1b0d
brunosala <connect@brunosala.com>
2021-05-24 14:29:21 -0500

    angular 11

--- src/app/portfolio-tiles/portfolio-tiles.component.spec.ts
+++ src/app/portfolio-tiles/portfolio-tiles.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PortfolioTilesComponent } from './portfolio-tiles.component';
 
@@ -6,7 +6,7 @@ describe('PortfolioTilesComponent', () => {
   let component: PortfolioTilesComponent;
   let fixture: ComponentFixture<PortfolioTilesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PortfolioTilesComponent ]
     })
--- src/app/return-home/return-home.component.spec.ts
+++ src/app/return-home/return-home.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ReturnHomeComponent } from './return-home.component';
 
@@ -6,7 +6,7 @@ describe('ReturnHomeComponent', () => {
   let component: ReturnHomeComponent;
   let fixture: ComponentFixture<ReturnHomeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReturnHomeComponent ]
     })
