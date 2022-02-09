df18163d8f53cdb19b37ac0913704363aec1b270
brunosala <connect@brunosala.com>
2022-02-09 07:56:55 -0600

    update resume + cv

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -4,29 +4,22 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   openResume() {
-    setTimeout(
-      function(){ 
-        window.open("assets/docs/Daniel Bruno Sala - Resume - 08122021.pdf");
-      }, 400
-    );
+    setTimeout(function () {
+      window.open('assets/docs/Daniel Bruno Sala - Resume - 02_22.pdf');
+    }, 300);
   }
 
   openCV() {
-    setTimeout(
-      function(){ 
-        window.open("assets/docs/Daniel Bruno Sala - CV - 08122021.pdf");
-      }, 400
-    );
+    setTimeout(function () {
+      window.open('assets/docs/Daniel Bruno Sala - CV - 02_22.pdf');
+    }, 300);
   }
 }
