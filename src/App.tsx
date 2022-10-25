194b5a346d364f538a99303814b9cbec3db475ac
Bruno Sala <connect@brunosala.com>
2022-10-25 14:00:48 -0500

    pause

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
+import Main from './views/Main';
 
 const App = () => {
   return (
     <div className='main'>
-      <div className='buttons'>
-        <button className='button is-primary'>Primary</button>
-        <button className='button is-link'>Link</button>
-        <button className='button is-info'>Info</button>
-        <button className='button is-success'>Success</button>
-        <button className='button is-warning'>Warning</button>
-        <button className='button is-danger'>Danger</button>
-        <button className='button is-black'>Black</button>
-        <button className='button is-white'>White</button>
-        <button className='button is-dark'>Dark</button>
-        <button className='button is-light'>Light</button>
-      </div>
+      <Main />
     </div>
   );
 };
---
+++ src/composites/Intro.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className='columns'>
+      <div className='column'></div>
+      <div className='column'></div>
+    </div>
+  );
+};
+
+export default Main;
