c9b2411c2749493f5a8e82a0bd463bd994b5775b
Bruno Sala <connect@brunosala.com>
2022-10-25 14:07:27 -0500

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './css/theme.min.css';
-import './css/theme.overrides.css';
+import './globals/theme.min.css';
+import './globals/theme.overrides.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
